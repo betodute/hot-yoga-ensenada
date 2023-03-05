1cf914abd439d60dd7798f83bbcc5bc305c1d097
Alberto Duarte <betodute@gmail.com>
2023-03-05 15:17:45 -0800

    react-router working
    please make sure you are in the root directory of your app before you install stuff that will save you two hours of your life in the future omg

--- hot-yoga-ensenada-frontend/src/App.js
+++ hot-yoga-ensenada-frontend/src/App.js
@@ -1,9 +1,26 @@
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import { Home } from './Home'
 import './App.css';
+
+const NotFound = () => {
+  return <h2>404 Not Found</h2>;
+}
 
 function App() {
   return (
-   <h1> Hot Yoga Ensenada </h1>
+    <Router>
+    <Link to="/">
+      <nav>
+        <h3 className="main-headline">Hot Yoga Ensenada</h3>
+      </nav>
+    </Link>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      
+      <Route element={NotFound} />
+    </Routes>  
+  </Router> 
   );
 }
 
---
+++ hot-yoga-ensenada-frontend/src/Home.js
@@ -0,0 +1,5 @@
+export const Home = () => {
+  return(
+    <h3> Hot Yoga Home Component Testing</h3>
+  )
+};
