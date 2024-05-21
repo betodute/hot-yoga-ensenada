d43a7f463cc6043e2d4f2f720be8956fbf662fd7
Alberto Duarte <betodute@gmail.com>
2024-05-20 17:19:50 -0700

    moving onto functionality
    reservation functionality

--- hot-yoga-ensenada-frontend/src/Headline.js
+++ hot-yoga-ensenada-frontend/src/Headline.js
@@ -1,9 +1,13 @@
 import './Headline.css';
+import homeImageOne from './images/original.png'
 
 export const Headline = () => {
   return (
     <div className='headline-wrapper'>
       <h2 className="headline">Hot Yoga Ensenada</h2>
+      <div className='image-wrapper text-center'>
+          <img className="home-image mx-auto d-block" src={homeImageOne}></img>
+        </div>
     </div>
   )
 }
--- hot-yoga-ensenada-frontend/src/Home.js
+++ hot-yoga-ensenada-frontend/src/Home.js
@@ -3,7 +3,6 @@ import { Reservation } from './Reservation'
 import { Profile } from './Profile'
 import { Logout } from './Logout'
 import { Footer } from './Footer'
-import homeImageOne from './images/original.png'
 import './Home.css'
 
 export const Home = () => {
@@ -12,11 +11,8 @@ export const Home = () => {
     <div className='container'>
       <div className='row'>
         <Headline className='col-12'/>
-        <div className='image-wrapper text-center'>
-          <img className="home-image mx-auto d-block" src={homeImageOne}></img>
-        </div>
         <Profile />
-        <Reservation today={new Date().getDay()} time={new Date().getHours()} className='col-12'/>
+        {/* <Reservation today={new Date().getDay()} time={new Date().getHours()} className='col-12'/> */}
         <Logout />
         <Footer />
       </div>
